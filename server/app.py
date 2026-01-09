988e4c754361ae902c333c965ba3c03d721e3e47
jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2026-01-09 15:08:04 +0300

    Lab done

--- server/app.py
+++ server/app.py
@@ -47,6 +47,33 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        data = request.get_json()
+
+        # Only update allowed attributes
+        allowed = {'name', 'image', 'price', 'is_in_stock'}
+        for key, value in data.items():
+            if key in allowed:
+                setattr(plant, key, value)
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return ('', 204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
